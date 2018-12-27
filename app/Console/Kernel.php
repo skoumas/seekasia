0c05e2913c047c0b1c2276bc76314c8014ebec80
Georgios Papanikolaou <skoumas@Georgioss-iMac.local>
2018-12-27 15:08:20 +0800

    Daily at specific time

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,7 +26,7 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')
         //          ->hourly();
-        $schedule->command('dailycheck')->daily();
+        $schedule->command('dailycheck')->dailyAt('13:00');
     }
 
     /**
